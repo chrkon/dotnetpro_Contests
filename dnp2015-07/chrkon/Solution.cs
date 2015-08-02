6e92eb1092ac188ce54e13fece2b081e263e2a40
Christof <chrkon@mail.de>
2015-08-02 08:44:17 +0200

    dnp2015-07 - Lösung von chrkon

---
+++ dnp2015-07/chrkon/Solution.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using contest.submission.contract;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+using System.Diagnostics;
+
+// 
+// Lösung für den Programmierwettbewerb der dotNetPro 2015/07
+// Programiert von Christof Konstantinopoulos 
+//
+// e-Mail: christof@konstantinopoulos.de
+//
+// Der Quelltext ist nicht zur Veröffentlichung freigegeben und 
+// darf nur für die Auswertung des Wettbewerbs verwendet werden.
+//
+// Für die Lösung wurde das Paket Math.net (http://numerics.mathdotnet.com/) genutzt. 
+//
+// 21.7.2015
+
+namespace contest.submission
+{
+  [Serializable]
+  public class Solution : IDnp1507Solution
+  {
+    private Products allproducts;
+    private ProductPackages allproductpackages;
+    
+    private Matrix<double> productMatrix; // die Zeilen definieren ein Paket, die Reihen stehen für ein Produkt
+    private Vector<double> prices; // Preise für jedes Produkt/Paket
+    private Vector<double> orderedProducts; // Bestellung (Besteht nur aus Produkten)
+    private Vector<double> packagedOrder; // Bestellung aufgeschlüsselt in Pakete und Produkte
+
+    private int productsAndPackagesCount = 0; 
+
+    public void Setup(Products products, ProductPackages productpackages)
+    {
+      this.allproducts = products;
+      this.allproductpackages = productpackages;
+
+      productsAndPackagesCount = products.Count + productpackages.Count;
+    
+      var product2PackageArray = new double[productsAndPackagesCount, products.Count];
+      var pricesArray = new double[productsAndPackagesCount];
+      var packagedOrderArray = new double[productsAndPackagesCount];
+
+      // Produktmatrix und Preisvectoraufbauen
+
+      var col = 0;
+      var row = 0;
+
+      var grundpreis = 0.0;
+
+      var productSet = new HashSet<Tuple<double, double, object>>();    
+
+      // Schritt 1 - Preis Index der Pakete ermitteln
+      foreach (var package in productpackages)
+      {
+          grundpreis = 0.0;
+          foreach (var price in package.Products)
+          {
+              var preis = products[price];
+              grundpreis += preis;
+          }
+          var preisIndex = (package.Price / grundpreis);
+          productSet.Add(new Tuple<double, double, object>(preisIndex, package.Price, package));
+      }
+
+      // Schritt 2 - Preis Index der Produkte ermitteln
+      foreach (var product in products)
+      {
+          grundpreis = product.Value;
+          var preisIndex = (product.Value / grundpreis);
+          productSet.Add(new Tuple<double, double, object>(preisIndex, product.Value, product));
+      }
+
+      // Schritt 3 - das ProduktSet sortieren und in eine Matrix überführen
+
+      var query = from p in productSet
+                  orderby p.Item1 ascending
+                  select p;
+        
+        row = 0;
+        foreach (var p in query)
+        {
+            col = 0;
+            foreach (var product in products)
+            {
+                if (p.Item3 is ProductPackage)
+                {
+                    foreach (var item in ((ProductPackage)p.Item3).Products)
+                    {
+                        if (item == product.Key)
+                        {
+                            product2PackageArray[row, col]++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (product.Key == ((KeyValuePair<string, int>)p.Item3).Key)
+                    {
+                        product2PackageArray[row, col]++;
+                    }
+                }
+                col++;
+            }
+            pricesArray[row] = p.Item2;
+            row++;
+        }
+
+        productMatrix = Matrix<double>.Build.DenseOfArray(product2PackageArray);
+        packagedOrder = Vector<double>.Build.DenseOfArray(packagedOrderArray);
+        prices = Vector<double>.Build.DenseOfArray(pricesArray);
+        orderedProducts = Vector<double>.Build.Dense(products.Count);
+    }
+    
+    public void Order(string productname)
+    {
+        if (!allproducts.ContainsKey(productname)) return;
+
+        // Bestelltes Produkt in orderedProducts einbauen
+        var col = 0;
+        foreach (var product in allproducts)
+        {
+            if (product.Key == productname)
+            {
+                orderedProducts[col]++;
+            }
+            col++;
+        }
+
+        Optimize();
+
+        if (SendPrice != null) SendPrice(CalculateTotalPrice());
+    }
+
+    private void Optimize()
+    {
+        var minimumList = new List<Tuple<double, Vector<Double>>>();
+
+        for (int p = 0; p < productsAndPackagesCount; p++)
+        {
+            var order = Vector<Double>.Build.DenseOfVector(orderedProducts);
+            var actualPackagedOrder = Vector<Double>.Build.Dense(productsAndPackagesCount);
+
+            var runs = 0;
+            var maxRuns = productMatrix.Row(p).Sum();
+
+            while (order.Sum() > 0 && runs <= maxRuns )
+            {
+                runs++;
+
+                var a = productMatrix.Multiply(order);
+
+                for (int i = 0; i < productsAndPackagesCount; i++)
+                {
+                    if (allproductpackages.Count>1 && i == p) continue; 
+
+                    if (a[i] > 0)
+                    {
+                        var row = productMatrix.Row(i);
+
+                        // Paket erkennen 
+                        while (OrderContainsProductCombination(order, row))
+                        {
+                            // Paket hinzufügen
+                            actualPackagedOrder[i]++;
+
+                            // zugehöhrige Produkte aus order entfernen
+                            for (int j = 0; j < order.Count; j++)
+                            {
+                                var itemCount = productMatrix.Row(i)[j];
+                                if (itemCount > 0 && order[j] >= itemCount)
+                                {
+                                    order[j] -= itemCount;
+                                }                                
+                            }
+                        }
+                    }
+                }
+            }
+            var price = actualPackagedOrder.PointwiseMultiply(prices).Sum();
+            if (runs <= maxRuns) minimumList.Add(new Tuple<double, Vector<double>>(price, actualPackagedOrder));
+        }
+        // Kombination mit dem gerigsten Preis verwenden.
+        var obj = minimumList.OrderBy(item => item.Item1).FirstOrDefault();
+        if (obj != null) packagedOrder = obj.Item2;
+    }
+
+    private bool OrderContainsProductCombination(Vector<double> order, Vector<double> packageContent)
+    {
+        var result = true;
+        for (int i  = 0; i < orderedProducts.Count; i++)
+        {
+            if (order[i] < packageContent[i]) result = false;
+        }
+        return result;
+    }
+
+    private int CalculateTotalPrice()
+    {
+        var preis =packagedOrder.PointwiseMultiply(prices).Sum();
+        Debug.WriteLine("Preis = {0}", preis);
+        return (int)preis;
+    }
+
+    public event Action<int> SendPrice;
+  }
+}
---
+++ dnp2015-07/chrkon/Test.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using contest.submission.contract;
+using contest.submission.Tests.Helper;
+using FluentAssertions;
+ 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+ 
+//
+// Die Tests wurden zum Teil aus dem Forum der DotNetPro übernommen
+// - Vielen Dank an M.Beetz
+//
+
+namespace contest.submission.Tests
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void Test_CK_einProdukt()
+        {
+            var sut = new Solution();
+
+            var products = new Products { { "Apfel", 10 } };
+
+            var success = false;
+
+            sut.Setup(products, new ProductPackages());
+
+            var price = 0;
+            sut.SendPrice += p =>
+            {
+                price = p;
+                Debug.WriteLine("{0},", price);
+            };
+
+            sut.Order("Apfel");
+            price.Should().Be(10);
+        }
+
+        [TestMethod]
+        public void Test_CK_einProduktZweiMal()
+        {
+            var sut = new Solution();
+
+            var products = new Products { { "Apfel", 10 } };
+
+            var success = false;
+
+            sut.Setup(products, new ProductPackages());
+
+            var price = 0;
+            sut.SendPrice += p =>
+            {
+                price = p;
+                Debug.WriteLine("{0},", price);
+            };
+
+            sut.Order("Apfel");
+            price.Should().Be(10);
+            sut.Order("Apfel");
+            price.Should().Be(20);
+        }
+
+        [TestMethod]
+        public void Test_CK_einProduktZweiMal_InsPaket()
+        {
+            var sut = new Solution();
+
+            var products = new Products { { "Apfel", 10 } };
+
+            var success = false;
+
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage(){ Price = 15, Products = { "Apfel", "Apfel" }},
+
+            };
+
+            sut.Setup(products, productPackages);
+
+            var price = 0;
+            sut.SendPrice += p =>
+            {
+                price = p;
+                Debug.WriteLine("{0},", price);
+            };
+
+            sut.Order("Apfel");
+            price.Should().Be(10);
+            sut.Order("Apfel");
+            price.Should().Be(15);
+        }
+
+        
+        [TestMethod]
+        public void Test_Forum()
+        {
+            var sut = new Solution();
+
+            var products = new Products {
+                { "VegMac", 4 },
+                { "BurgerMac", 1 },
+                { "WoodMac", 3 },
+                { "MacMac", 5 },
+                { "PommesPommes", 2 },
+                { "Pommes", 1 },
+                { "PommesGr", 1 },
+                { "Cola", 3 }
+            };
+
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage(){ Price = 1, Products = { "MacMac", "Cola" }},
+                new ProductPackage(){ Price = 10, Products = { "BurgerMac", "WoodMac", "Pommes", "Cola" }},
+                new ProductPackage(){ Price = 7, Products = { "VegMac", "Cola", "Pommes" }},
+                new ProductPackage(){ Price = 7, Products = { "VegMac", "Cola", "PommesGr" }},
+                new ProductPackage(){ Price = 2, Products = { "Cola", "PommesGr" }},
+                new ProductPackage(){ Price = 1, Products = { "VegMac", "Cola", "Pommes", "Pommes" }}
+            };
+
+
+
+            sut.Setup(products, productPackages);
+
+            var price = 0;
+            sut.SendPrice += p =>
+            {
+                price = p;
+                Debug.WriteLine("{0},",price);
+            };
+
+            sut.Order("VegMac");
+            price.Should().Be(4);
+            sut.Order("VegMac");
+            price.Should().Be(8);
+            sut.Order("VegMac");
+            price.Should().Be(12);
+            sut.Order("Pommes");
+            price.Should().Be(13);
+            sut.Order("Cola");
+            price.Should().Be(15); // P3 + 2xVegMac
+            sut.Order("Pommes");
+            price.Should().Be(9); // P3 + P6 + 1xVegMac
+            sut.Order("PommesGr");
+            price.Should().Be(10); // P4 + P6 + 1xPommes
+            sut.Order("MacMac");
+            price.Should().Be(15); // P4 + P6 + 1xPommes + 1xMacMac
+            sut.Order("WoodMac");
+            price.Should().Be(18); // P4 + P6 + 1xPommes + 1xMacMac + 1xWoodMac
+            sut.Order("BurgerMac");
+            price.Should().Be(19); // P4 + P6 + 1xPommes + 1xMacMac + 1xWoodMac + 1xBurgeMac
+            sut.Order("MacMac");
+            price.Should().Be(24); // P4 + P6 + 1xPommes + 2xMacMac + 1xWoodMac + 1xBurgeMac
+            sut.Order("PommesPommes");
+            price.Should().Be(26); // P4 + P6 + 1xPommes + 2xMacMac + 1xWoodMac + 1xBurgeMac + PommesPommes
+            sut.Order("Pommes");
+            price.Should().Be(27); // P4 + P6 + 2xPommes + 2xMacMac + 1xWoodMac + 1xBurgeMac + PommesPommes
+            sut.Order("Cola");
+            price.Should().Be(23); // P1 + P4 + P6 + 2xPommes + 1xMacMac + 1xWoodMac + 1xBurgeMac + PommesPommes
+        
+        }
+
+
+        [TestMethod]
+        public void Test_switch_to_another_package()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}, {"B", 20}, {"C", 30}};
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage {Price = 29, Products = new List<string> {"A", "B"}},
+                new ProductPackage {Price = 48, Products = new List<string> {"B", "C"}},
+            };
+ 
+            sut.Setup(products, productPackages);
+ 
+            var price = 0;
+            sut.SendPrice += p => price = p;
+ 
+            sut.Order("A");
+            sut.Order("B");
+            price.Should().Be(29, "there is a package for A and B with price 29.");
+ 
+            sut.Order("C");
+ 
+            price.Should().Be(10 + 48, "it is cheaper to take package B & C");
+        }
+ 
+        [TestMethod]
+        public void Test_if_pitfall_package_is_ignored()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}};
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage {Price = 21, Products = new List<string> {"A", "A"}}
+            };
+ 
+            sut.Setup(products, productPackages);
+ 
+            var price = 0;
+            sut.SendPrice += p => price = p;
+ 
+            sut.Order("A");
+            price.Should().Be(10);
+ 
+            sut.Order("A");
+            price.Should().Be(2 * 10, "package is a pitfall");
+        }
+ 
+        [TestMethod]
+        public void Test_quantity_discount()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}};
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage {Price = 19, Products = new List<string> {"A", "A"}},
+                new ProductPackage {Price = 27, Products = new List<string> {"A", "A", "A"}},
+            };
+ 
+            sut.Setup(products, productPackages);
+ 
+            var price = 0;
+            sut.SendPrice += p => price = p;
+ 
+            sut.Order("A");
+            price.Should().Be(10);
+ 
+            sut.Order("A");
+            price.Should().Be(19);
+ 
+            sut.Order("A");
+            price.Should().Be(27);
+ 
+            sut.Order("A");
+            price.Should().Be(27 + 10);
+ 
+            sut.Order("A");
+            price.Should().Be(27 + 19);
+ 
+            sut.Order("A");
+            price.Should().Be(2 * 27);
+        }
+ 
+        [TestMethod]
+        public void Test_impossible_product()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}, {"B", 20}, {"C", 30}};
+ 
+            var success = false;
+ 
+            sut.Setup(products, new ProductPackages());
+ 
+            sut.SendPrice += p => success = true;
+ 
+            sut.Order("D");
+ 
+            success.Should().BeFalse();
+        }
+ 
+        private int count = 0;
+
+        [TestMethod]       
+        public void Test_performance()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}, {"B", 20}, {"C", 30}};
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage {Price = 29, Products = new List<string> {"A", "B"}},
+                new ProductPackage {Price = 30, Products = new List<string> {"A", "A", "A", "A"}},
+                new ProductPackage {Price = 48, Products = new List<string> {"B", "C"}},
+                new ProductPackage {Price = 57, Products = new List<string> {"A", "B", "C"}}
+            };
+ 
+            sut.Setup(products, productPackages);
+            sut.SendPrice += price => Console.WriteLine("{0,3}. Order, Price: {1,5}", count++, price);
+
+            //sut.OrderMultiple(50, "A");
+            //sut.OrderMultiple(5, "B");
+            //sut.OrderMultiple(3, "C");
+            sut.OrderMultiple(40, "A");
+            sut.OrderMultiple(40, "B");
+            sut.OrderMultiple(40, "C");
+
+        }
+ 
+        [TestMethod]
+        public void Test_order_where_it_is_not_better_to_take_highest_discount()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products { { "A", 10 } };
+            var productPackages = new ProductPackages
+            {
+                new ProductPackage {Price = 47, Products = new List<string> {"A", "A", "A", "A", "A"}}, // discount 3/5 pp
+                new ProductPackage {Price = 38, Products = new List<string> {"A", "A", "A", "A"}} // discount 1/2 pp
+            };
+ 
+            var price = 0;
+            sut.Setup(products, productPackages);
+            sut.SendPrice += p => price = p;
+ 
+            sut.OrderMultiple(8, "A");
+ 
+            price.Should().Be(2 * 38);
+        }
+ 
+        [TestMethod]
+        public void Test_order_single_product_twice()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}};
+            var price = 0;
+ 
+            sut.Setup(products, new ProductPackages());
+ 
+            sut.SendPrice += p => price = p;
+ 
+            sut.Order("A");
+            sut.Order("A");
+ 
+            price.Should().Be(20);
+        }
+ 
+        [TestMethod]
+        public void Test_order_with_too_big_package()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 1}};
+            var price = 0;
+ 
+            sut.Setup(products, new ProductPackages {new ProductPackage {Price = 6, Products = Enumerable.Repeat("A", 8).ToList()}});
+ 
+            sut.SendPrice += p => price = p;
+ 
+            sut.OrderMultiple(7, "A");
+ 
+            price.Should().Be(7, "6 would be to many As");
+        }
+ 
+        [TestMethod]
+        public void Test_order_single_product()
+        {
+            var sut = new Solution();
+ 
+            var products = new Products {{"A", 10}};
+            var price = 0;
+ 
+            sut.Setup(products, new ProductPackages());
+ 
+            sut.SendPrice += p => price = p;
+ 
+            sut.Order("A");
+ 
+            price.Should().Be(10);
+        }
+    }
+}
+ 
+ 
+namespace contest.submission.Tests.Helper
+{
+    public static class SolutionExtension
+    {
+        public static void OrderMultiple(this Solution solution, int quantity, string productName)
+        {
+            for (int i = 0; i < quantity; i++)
+                solution.Order(productName);
+        }
+    }
+}
+
